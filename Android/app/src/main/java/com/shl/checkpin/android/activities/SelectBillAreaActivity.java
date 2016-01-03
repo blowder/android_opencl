366ac308673b7d12908cb9dc007cbb709c7bda51
Sesshoumaru <blowder@mail.ru>
2016-01-03 17:22:37 +0200

    Dots for bill select were added to canvas

--- Android/app/src/main/java/com/shl/checkpin/android/activities/SelectBillAreaActivity.java
+++ Android/app/src/main/java/com/shl/checkpin/android/activities/SelectBillAreaActivity.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.graphics.*;
 import android.os.Bundle;
 import android.os.Environment;
-import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
 import com.shl.checkpin.android.canvas.Circle;
@@ -15,15 +14,16 @@ import com.shl.checkpin.android.utils.FSFileLocator;
 import com.shl.checkpin.android.utils.FileLocator;
 import org.opencv.core.Size;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by sesshoumaru on 29.12.15.
  */
 public class SelectBillAreaActivity extends Activity implements View.OnTouchListener {
     private FileLocator appFileLocator = new FSFileLocator(FSFileLocator.FSType.EXTERNAL);
     String fileName = "bill.jpg";
-    String COLOR = "SelectBillAreaActivity.color";
     private DrawView drawView = null;
-    private int color = Color.GREEN;
     private int canvasWidth;
     private int canvasHeight;
     Bitmap image;
@@ -31,50 +31,79 @@ public class SelectBillAreaActivity extends Activity implements View.OnTouchList
 
     private int threshold = 50;
     private int circleRadius = 30;
+    List<Circle> circles = new ArrayList<Circle>();
+    //Circle circle = new Circle(200, 200, circleRadius);
 
-    Circle circle = new Circle(200, 200, circleRadius);
+    private void initCircles() {
+        initScreenDimension();
+        int aThirdOfWidth = canvasWidth / 3;
+        int aThirdOfHeight = canvasHeight / 3;
+        Circle tl = new Circle(aThirdOfWidth, aThirdOfHeight, circleRadius);
+        Circle tr = new Circle(aThirdOfWidth * 2, aThirdOfHeight, circleRadius);
+        Circle bl = new Circle(aThirdOfWidth, aThirdOfHeight * 2, circleRadius);
+        Circle br = new Circle(aThirdOfWidth * 2, aThirdOfHeight * 2, circleRadius);
+
+        tl.setNext(tr);
+        tr.setNext(br);
+        br.setNext(bl);
+        bl.setNext(tl);
+
+        circles.add(tr);
+        circles.add(br);
+        circles.add(bl);
+        circles.add(tl);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (savedInstanceState != null) {
+        initCircles();
+
+        //example of retrieving saved values
+        /*if (savedInstanceState != null) {
             color = savedInstanceState.getInt(COLOR);
-        }
+        }*/
+
         image = BitmapFactory.decodeFile(appFileLocator.locate(Environment.DIRECTORY_PICTURES, fileName).getAbsolutePath());
         drawView = new DrawView(this);
         drawView.setOnTouchListener(this);
         setContentView(drawView);
 
-        Point size = AndroidUtils.getScreenDimension(this);
-
-        canvasWidth = size.x;
-        canvasHeight = size.y;
         Size dimension = OpenCvUtils.getScaledDimension(new Size(image.getWidth(), image.getHeight()), new Size(canvasWidth, canvasHeight));
         image = Bitmap.createScaledBitmap(image, (int) dimension.width, (int) dimension.height, true);
-
     }
 
-    @Override
+    private void initScreenDimension() {
+        Point size = AndroidUtils.getScreenDimension(this);
+        canvasWidth = size.x;
+        canvasHeight = size.y;
+    }
+
+    //example of saving result
+    /*@Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putInt(COLOR, color);
-
-
-    }
+    }*/
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
         float x = event.getX();
         float y = event.getY();
         if (MotionEvent.ACTION_MOVE == event.getAction()) {
-            if (x < circle.getX() + threshold
-                    && x > circle.getX() - threshold
-                    && y < circle.getY() + threshold
-                    && y > circle.getY() - threshold) {
-                circle.setX(x);
-                circle.setY(y);
-                drawView.invalidate();
+            List<Circle> touchedCircles = new ArrayList<Circle>();
+            for (Circle circle : circles)
+                if (x < circle.getX() + threshold
+                        && x > circle.getX() - threshold
+                        && y < circle.getY() + threshold
+                        && y > circle.getY() - threshold)
+                    touchedCircles.add(circle);
+
+            if (touchedCircles.size() != 0) {
+                touchedCircles.get(0).setX(x);
+                touchedCircles.get(0).setY(y);
             }
+            drawView.invalidate();
         }
         return true;
     }
@@ -91,7 +120,8 @@ public class SelectBillAreaActivity extends Activity implements View.OnTouchList
             canvas.drawBitmap(image, 0, 0, null);
             //image.recycle();
             //image=null;
-            circle.draw(canvas);
+            for (Circle circle : circles)
+                circle.draw(canvas);
 
         }
     }
