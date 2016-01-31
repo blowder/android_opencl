bb05f4bb143eec2450d7c5a0673285dc5a753f05
blowder <blowder@mail.ru>
2016-01-31 22:37:21 +0200

    icons was added to history card background

--- Android/app/src/main/java/com/shl/checkpin/android/activities/NewHistoryActivity.java
+++ Android/app/src/main/java/com/shl/checkpin/android/activities/NewHistoryActivity.java
@@ -3,6 +3,9 @@ package com.shl.checkpin.android.activities;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
@@ -14,10 +17,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
+import android.graphics.Canvas;
 import com.shl.checkpin.android.R;
 import com.shl.checkpin.android.jobs.ImageThumbnailCreateTask;
 import com.shl.checkpin.android.jobs.ImageUploadTask;
 import com.shl.checkpin.android.utils.*;
+import android.graphics.Matrix;
+
 
 import java.io.File;
 import java.util.*;
@@ -35,6 +41,8 @@ public class NewHistoryActivity extends Activity {
 
     private RecyclerView listView;
     private RecyclerView.LayoutManager mLayoutManager;
+    private static Bitmap sendIcon;
+    private static Bitmap deleteIcon;
 
 
     @Override
@@ -44,6 +52,7 @@ public class NewHistoryActivity extends Activity {
         listView = (RecyclerView) findViewById(R.id.history_list);
         files = getImages();
         this.sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        
 
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
@@ -57,6 +66,43 @@ public class NewHistoryActivity extends Activity {
 
         listView.setAdapter(adapter);
         ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+
+            @Override
+            public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+                if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+
+                    View itemView = viewHolder.itemView;
+                    int iconSize = (int)((itemView.getTop()-itemView.getBottom())/2);
+                    
+                    if (dX < 0) {                      
+                        if(sendIcon==null)
+                            sendIcon = getBitmapById(R.raw.send_icon, iconSize, iconSize);                            
+                        
+                        int drawableX = itemView.getRight()-(sendIcon.getWidth() + sendIcon.getWidth()/4);
+                        int drawableY = itemView.getTop() + sendIcon.getWidth()/2;                        
+                        c.drawBitmap(sendIcon, drawableX, drawableY, null);                       
+                    }
+                    if (dX > 0) {
+                        if(deleteIcon==null)
+                            deleteIcon = getBitmapById(R.raw.delete_icon, iconSize, iconSize);                                                          
+                        
+                        int drawableX = itemView.getLeft() + deleteIcon.getWidth()/4;
+                        int drawableY = itemView.getTop() + deleteIcon.getWidth()/2;                        
+                        c.drawBitmap(deleteIcon, drawableX, drawableY, null);                       
+                    }
+
+                    super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+                }
+            }
+            private Bitmap getBitmapById(int id, int width, int height){
+                Bitmap result = BitmapFactory.decodeResource(getApplicationContext().getResources(), id);                        
+                result = Bitmap.createScaledBitmap(result, width, height, false);                                                   
+                //TODO: remove this spyke
+                Matrix matrix = new Matrix();
+                matrix.postRotate(180);
+                return Bitmap.createBitmap(result , 0, 0, result.getWidth(), result.getHeight(), matrix, true);
+            }
+
 
             @Override
             public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
@@ -74,7 +120,7 @@ public class NewHistoryActivity extends Activity {
                     if (AndroidUtils.isInetConnected(NewHistoryActivity.this) && sharedPreferences.getBoolean(Constants.SENT_TOKEN_TO_SERVER, false)) {
                         String gcmToken = sharedPreferences.getString(Constants.GCM_TOKEN, "");
                         new ImageUploadTask(NewHistoryActivity.this, AndroidUtils.getPhoneNumber(NewHistoryActivity.this), gcmToken)
-                                .executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, source);
+                        .executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, source);
                     } else {
                         AndroidUtils.toast(NewHistoryActivity.this, "Sorry there is no Internet connection or you try to send unexisted file", Toast.LENGTH_LONG);
                     }
@@ -138,12 +184,13 @@ public class NewHistoryActivity extends Activity {
         for (File file : appFileLocator.locate(Environment.DIRECTORY_PICTURES))
             if (file.getName().matches(pattern))
                 files.add(file);
-        Collections.sort(files, new Comparator<File>() {
-            @Override
-            public int compare(File lhs, File rhs) {
-                return new Date(rhs.lastModified()).compareTo(new Date(lhs.lastModified()));
-            }
-        });
-        return files;
-    }
+            Collections.sort(files, new Comparator<File>() {
+                @Override
+                public int compare(File lhs, File rhs) {
+                    return new Date(rhs.lastModified()).compareTo(new Date(lhs.lastModified()));
+                }
+            });
+            return files;
+        }
+       
 }
